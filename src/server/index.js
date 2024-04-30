a0b2fde2516d2ebb905297277d9e84f5c3398f80
Leo Brad <>
2024-04-30 14:08:38 +0800

    [Change] Change update check time.

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ https.createServer({
     title: 'Fulmination',
     content: 'Fulmination document website.',
     fonts: ['ttf'],
-    develope: true,
+    develope: false,
   });
   await commonHttp.process(req, res);
 }).listen(8000);
