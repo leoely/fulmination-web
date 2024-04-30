088c6af39b1df12d7ee12aba247819fb9e3ee04d
Leo Brad <>
2024-04-30 21:37:22 +0800

    [Add] favico is null logic.

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,16 @@ import https from 'https';
 import fs from 'fs';
 import { CommonHttp, } from 'manner.js/server';
 
+const commonHttp = new CommonHttp({
+  title: 'Fulmination',
+  content: 'Fulmination document website.',
+  fonts: ['ttf'],
+  develope: true,
+});
+
 https.createServer({
   key: fs.readFileSync('asset/fulmination-web-key.pem'),
   cert: fs.readFileSync('asset/fulmination-web-cert.pem'),
 }, async (req, res) => {
-  const commonHttp = new CommonHttp({
-    title: 'Fulmination',
-    content: 'Fulmination document website.',
-    fonts: ['ttf'],
-    develope: true,
-  });
   await commonHttp.process(req, res);
 }).listen(8000);
