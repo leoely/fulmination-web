e94f49db7b1a6cfe00cb69f1be3e4f2f5db64921
Leo Ely <>
2025-08-21 18:15:11 +0800

    [Add] Add symbol-related documentation.

--- src/client/script/page/Reference/index.js
+++ src/client/script/page/Reference/index.js
@@ -1,51 +1,75 @@
 import React from 'react';
 import Page from '~/client/script/component/Page';
+import H2 from '~/client/script/component/H2';
+import HighLight from '~/client/script/component/HighLight';
 import Text from '~/client/script/component/Text';
 import Formats from '~/client/script/component/Formats';
 import Format from '~/client/script/component/Format';
+import Styles from '~/client/script/component/Styles';
+import Style from '~/client/script/component/Style';
 import style from './index.module.css';
 
 class Reference extends Page {
   render() {
     return (
       <div className={style.reference}>
+        <H2>#Symbol "+"</H2>
         <Text>
-          ctf file start with "(+)" or "[+]"."(+)" which mean add a text."[+]"
-          which add a passage.Text and passage difference is text is one line
-          text and passage is multi-line text.
+          The symbol <HighLight>"+"</HighLight> in fulmination grammar is at the
+          beginning of the grammar.<HighLight>"+"</HighLight> is semantic,
+          meaning to add new text or a group of paragraph here.When adding a
+          text or paragraph,you can use<HighLight>"+"</HighLight>to continue
+          adding text or paragraphs of other colors or styles.
         </Text>
         <Formats>
-          <Format>(+) format1; format2; format3: text section</Format>
-        </Formats>
-        <Formats>
-          <Format>[+] format1; format2; format3: text section</Format>
-          <Format>| passage section1</Format>
-          <Format>| passage section2</Format>
+          <Format format="fulmiation">(+) bold: add a fulmination text.</Format>
+          <Format format="fulmiation">(+) bold: symbol """+"" sign in front.</Format>
         </Formats>
         <Text>
-          Then is format which belong to text and passage.Format is come from
-          chalk project.Because fulmination use chalk color text."format1;
-          format2; format3; ...; formatn:" syntax.Each format is correspond
-          a chalk style.";" is each format delimiter.And end with ":".Chalk
-          document website is https://github.com/chalk/chalk.
+          The following is the corresponding terminal effect display:
         </Text>
+        <Styles>
+          <Style styls={['bold', 'inline']}>add a fulmination text.symbol "+" sign in front</Style>
+          <Style styls={['inline', 'black', 'bgWhite', 'bold']}>%</Style>
+        </Styles>
+        <H2>#Symbol "(" and ")"</H2>
+        <Text>
+          Parentheses must be used with the symnol<HighLight>"+"</HighLight> and
+          cannot appear alone.<HighLight>"(+)"</HighLight> is the only way to
+          combine text.It adds a text with color and style.By default,the text
+          does not wrap.If the terminal does not have a default lien break,the
+          <HighLight>"%"</HighLight> symbol will be used to supplement the
+          description.
+       </Text>
         <Formats>
-          <Format>(+) bold; dim; underline: text section</Format>
+          <Format format="fulmiation">(+) green; bold: this is a fulmination text.</Format>
         </Formats>
         <Text>
-          The next is text part.As for text is follow by formats.As for passage
-          is delimiter with "|".When text need split into two line which need
-          use "&" as delimiter.
+          The following is the corresponding terminal effect display:
+        </Text>
+        <Styles>
+          <Style styls={['bold', 'green', 'inline']}>this is a fulmination text.</Style>
+          <Style styls={['inline', 'black', 'bgWhite', 'bold']}>%</Style>
+        </Styles>
+        <H2>#Symbol "[" and "]"</H2>
+        <Text>
+          Brackets and parenthese are similar,and the <HighLight>"+"</HighLight>
+          symbol combination indicates a group of paragraphs.
         </Text>
         <Formats>
-          <Format>[+] dim:</Format>
-          <Format>| passage section1</Format>
-          <Format>| passage section2</Format>
+          <Format format="fulmiation">[+] blue:</Format>
+          <Format format="fulmiation">| This is a line of paragraph text</Format>
+          <Format format="fulmiation">| This is a line of paragraph text</Format>
+          <Format format="fulmiation">| This is a line of paragraph text</Format>
         </Formats>
-        <Formats>
-          <Format>(+) bold; green: text section1 (+) bold: text section2 &</Format>
-          <Format>(+) red: text section3 (+) bold: text section4</Format>
-        </Formats>
+        <Text>
+          The following is the corresponding terminal effect display:
+        </Text>
+        <Styles>
+          <Style styls={['blue']}>This is a line of paragraph text</Style>
+          <Style styls={['blue']}>This is a line of paragraph text</Style>
+          <Style styls={['blue']}>This is a line of paragraph text</Style>
+        </Styles>
       </div>
     );
   }
